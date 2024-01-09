2700e30ab659ceacac5d1b46a46c3bdba10bffa1
KaterynaKocheharova <kot41901@gmail.com>
2024-01-09 15:06:58 +0200

    updated variable name

--- js/task-4.js
+++ js/task-4.js
@@ -11,7 +11,7 @@ users.filter(user => user.gender === gender)
 
 // =============================================== Перевірка ==================================================
 
-const allUsers = [
+const allUsers2 = [
     {
     name: "Moore Hensley",
     gender: "male",
@@ -49,9 +49,9 @@ const allUsers = [
   }
 ];
 
-console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
+console.log(getTotalBalanceByGender(allUsers2, "male")); // 12053
 
-console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
+console.log(getTotalBalanceByGender(allUsers2, "female")); // 8863
 
 
 
